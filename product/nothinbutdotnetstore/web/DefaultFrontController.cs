c985fab6b5db9958fc834a48a568b9ccacc64106
unknown <arielm@.(none)>
2010-07-27 21:52:24 -0400

    front controller change

--- product/nothinbutdotnetstore/web/DefaultFrontController.cs
+++ product/nothinbutdotnetstore/web/DefaultFrontController.cs
@@ -4,9 +4,17 @@ namespace nothinbutdotnetstore.web
 {
     public class DefaultFrontController : FrontController
     {
+        private CommandRegistry commandRegistry;
+
+        public DefaultFrontController(CommandRegistry commandRegistry)
+        {
+            this.commandRegistry = commandRegistry;
+        }
+
         public void process(Request request)
         {
-            throw new NotImplementedException();
+           var commandProcessor = commandRegistry.get_command_that_can_handle(request);
+           commandProcessor.process(request);
         }
     }
 }
