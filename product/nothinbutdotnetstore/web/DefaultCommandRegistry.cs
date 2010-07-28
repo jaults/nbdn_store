25f80f1216cb1097b8e366b623ab63b0a47e8505
Chris Farmer <chris.farmer@gmail.com>
2010-07-27 19:07:15 -0700

    command registry

--- product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
@@ -1,12 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace nothinbutdotnetstore.web
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
+        private IEnumerable<RequestCommand> commands;
+
+        public DefaultCommandRegistry(IEnumerable<RequestCommand> commands)
+        {
+            this.commands = commands;
+        }
+
         public RequestCommand get_command_that_can_handle(Request request)
         {
-            throw new NotImplementedException();
+            return commands.Single(command => command.can_process(request));
         }
     }
 }
