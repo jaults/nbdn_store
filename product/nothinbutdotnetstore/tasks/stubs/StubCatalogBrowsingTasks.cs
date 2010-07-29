0f8427ff5926c8e79507350c55c924443a7e09a7
Chris Farmer <chris.farmer@gmail.com>
2010-07-28 17:58:27 -0700

    refactor stubs

--- product/nothinbutdotnetstore/tasks/stubs/StubCatalogBrowsingTasks.cs
+++ product/nothinbutdotnetstore/tasks/stubs/StubCatalogBrowsingTasks.cs
@@ -9,21 +9,36 @@ namespace nothinbutdotnetstore.tasks.stubs
     {
         public IEnumerable<Department> get_the_main_departments()
         {
-            return
-                Enumerable.Range(1, 100).Select(
-                    x => new Department {name = x.ToString("Department 0"), has_sub_departments = x%2 == 0});
+            return get_collection_of(100,
+                                     x =>
+                                     new Department {name = x.ToString("Department 0"), has_sub_departments = x%2 == 0});
+       
         }
 
         public IEnumerable<Product> get_products_for_department(Department department)
         {
-            return
-                Enumerable.Range(1, 10).Select(
-                    x => new Product() { name = x.ToString("Product 0"), description = x.ToString("Product 0 Description"), price = x.ToString("$0")});
+            return get_collection_of(10,
+                                     x =>
+                                     new Product()
+                                         {
+                                             name = x.ToString("Product 0"),
+                                             description = x.ToString("Product 0 Description"),
+                                             price = x.ToString("$0")
+                                         });
+            
         }
 
         public IEnumerable<Department> get_the_sub_departments_in(Department department)
         {
-            return Enumerable.Range(1, 10).Select(x => new Department { name = x.ToString("Sub Department 0") });
+            return get_collection_of(10, x => new Department {name = x.ToString("Sub Department 0")});
+           
         }
+
+
+        private IEnumerable<ReturnType> get_collection_of<ReturnType>(int quantity, Func<int, ReturnType> creator)
+        {
+            return Enumerable.Range(1, quantity).Select(creator);
+        }
+
     }
 }
